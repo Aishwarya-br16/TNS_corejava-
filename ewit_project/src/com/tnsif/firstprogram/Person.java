b006dd85336fde1c74afe3d0950106f625952cd9
OM <OM@hp>
2025-10-09 13:03:10 +0530

    First commit

---
+++ ewit_project/src/com/tnsif/firstprogram/Person.java
@@ -0,0 +1,38 @@
+package com.tnsif.firstprogram;
+
+public class Person {
+	
+	private String personName;
+	protected int personAge;
+	String personCity;
+	
+	public String getPersonName() {
+		return personName;
+	}
+	
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+	
+	public int getPersonAge() {
+		return personAge;
+	}
+	
+	public void setPersonAge(int personAge) {
+		this.personAge = personAge;
+	}
+	
+	public String getPersonCity() {
+		return personCity;
+	}
+	
+	public void setPersoncity(String personCity) {
+		this.personCity = personCity;
+	}
+	
+	void display() {
+		System.out.println(personName +" "+ personAge +" "+ personCity);
+		
+	}
+
+}
