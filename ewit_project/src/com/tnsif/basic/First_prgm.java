12ca64c45ccda655b9b906e4c11d6e1a812cb57d
OM <OM@hp>
2025-10-09 12:58:13 +0530

    Basic

---
+++ ewit_project/src/com/tnsif/basic/First_prgm.java
@@ -0,0 +1,14 @@
+package com.tnsif.basic;
+
+public class First_prgm {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		int num1 = 10;
+		int num2 = 20;
+		int sum = num1 + num2;
+		System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+	}
+
+}
---
+++ ewit_project/src/com/tnsif/basic/Second_prgm.java
@@ -0,0 +1,10 @@
+package com.tnsif.basic;
+
+public class Second_prgm {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World from SecondPrgm");
+
+	}
+
+}
